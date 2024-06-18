3f07ff1c132b0910a513a2bd10b34aaea67bbc13
Eleftherios Kalampakas <leeburton01@gmail.com>
2024-06-18 13:25:36 +0200

    Solved Lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,29 +1,42 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
+    return Math.floor(this.currentTime % 60);
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    let string = "0" + value;
+    return string.slice(-2);
     // ... your code goes here
   }
 
   stop() {
-    // ... your code goes here
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
   }
 
   reset() {
+    this.currentTime = 0;
     // ... your code goes here
   }
 
